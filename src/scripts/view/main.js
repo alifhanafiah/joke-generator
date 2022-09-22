afb878fd15202f8c57085db8c3a0ace157d064ae
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-09-22 07:55:55 +0700

    done

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -3,6 +3,17 @@ const axios = require('axios').default;
 const main = () => {
   const baseUrl = 'https://v2.jokeapi.dev/joke/Programming,Dark,Pun';
 
+  // loading spinner css
+  const loader = document.querySelector('#loading');
+  const displayLoading = () => {
+    loader.classList.add('display');
+  };
+
+  // hiding loading
+  const hideLoading = () => {
+    loader.classList.remove('display');
+  };
+
   const renderJoke = (jokeObject) => {
     const jokeSetup = document.querySelector('#joke-setup');
     const jokeDelivery = document.querySelector('#joke-delivery');
@@ -21,8 +32,11 @@ const main = () => {
 
   const getJoke = async () => {
     try {
+      displayLoading();
       const response = await axios.get(`${baseUrl}`);
       const responseData = await response.data;
+
+      hideLoading();
 
       if (responseData.error) {
         showResponseMessage(responseData.error);
